eaabe88a9d2d55d4b038ce55d1b26b75adf5d0f7
gouravgoel <121gouravkolkata@gmail.com>
2021-01-22 19:55:05 +0530

    added get user and update user by id functionality(admin only)

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -18,7 +18,10 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
-  USER_LIST_RESET
+  USER_LIST_RESET,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
@@ -197,6 +200,38 @@ export const listUsers = () => async (dispatch, getState) => {
   } catch (err) {
     dispatch({
       type: USER_LIST_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message
+    })
+  }
+}
+
+export const deleteUser = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DELETE_REQUEST
+    })
+
+    const {
+      userLogin: { userInfo }
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    await axios.delete(`/api/users/${id}`, config)
+
+    dispatch({
+      type: USER_DELETE_SUCCESS
+    })
+  } catch (err) {
+    dispatch({
+      type: USER_DELETE_FAIL,
       payload:
         err.response && err.response.data.message
           ? err.response.data.message
